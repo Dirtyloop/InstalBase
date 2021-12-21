22cc537bf1d8608d368ce50a77fbaa4741c80ab8
Michal Nowakowski <michal.nowakowski85@gmail.com>
2021-12-21 15:15:51 +0100

    initial commit

---
+++ src/main/java/com/komfortcieplny/InstalBase/InstalBaseApplication.java
@@ -0,0 +1,13 @@
+package com.komfortcieplny.InstalBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstalBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstalBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/komfortcieplny/InstalBase/InstalBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komfortcieplny.InstalBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstalBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

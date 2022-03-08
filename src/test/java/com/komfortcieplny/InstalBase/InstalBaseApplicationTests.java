159e68c4745302d3385f4168d356ecd96323ab4c
Michal Nowakowski <michal.nowakowski85@gmail.com>
2022-03-08 22:06:57 +0100

    deploy on server

--- src/test/java/com/komfortcieplny/InstalBase/InstalBaseApplicationTests.java
+++ src/test/java/com/komfortcieplny/InstalBase/InstalBaseApplicationTests.java
@@ -1,13 +1,12 @@
 package com.komfortcieplny.InstalBase;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InstalBaseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
